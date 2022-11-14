fc2d86608f1a1e8ff9fcb711ae577721a2fbd5ee
Leo Candra <leocandra1609@gmail.com>
2022-11-14 15:00:36 +0700

    test commit 2

---
+++ lib/presentation/pages/signup_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatelessWidget{
+  const SignUpPage({Key? key}):super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: const <Widget>[
+          Text('data'),
+        ],
+      ),
+    );
+  }
+}
