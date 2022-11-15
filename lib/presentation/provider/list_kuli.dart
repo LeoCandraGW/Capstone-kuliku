dbc85443027ebbb214f154fe4aa7e9cf3a3a7378
Leo Candra <leocandra1609@gmail.com>
2022-11-15 12:43:04 +0700

    list kuli

---
+++ lib/presentation/provider/list_kuli.dart
@@ -0,0 +1,54 @@
+class ListKuli {
+  String name;
+  String alamat;
+  String skill;
+  String dailysal;
+  String imageAsset;
+
+  ListKuli({
+    required this.name,
+    required this.alamat,
+    required this.skill,
+    required this.dailysal,
+    required this.imageAsset,
+  });
+}
+
+var listKuliku = [
+  ListKuli(
+      name: 'Maria',
+      alamat: 'Surabaya, Jawa Timur',
+      skill: 'Renovate, HomeCare',
+      dailysal: 'Rp. 165.000,00',
+      imageAsset: 'image/kuliku1.png'),
+  ListKuli(
+      name: 'Naruto',
+      alamat: 'Bekasi, Jawa Barat',
+      skill: 'Interior, Build, Yard',
+      dailysal: 'Rp. 155.000,00',
+      imageAsset: 'image/kuliku2.png'),
+  ListKuli(
+      name: 'Denji',
+      alamat: 'Malang, Jawa Timur',
+      skill: 'Interior, Room Makeover',
+      dailysal: 'Rp. 135.000,00',
+      imageAsset: 'image/kuliku3.png'),
+  ListKuli(
+      name: 'Emilia',
+      alamat: 'Medan, Sumatra Utara',
+      skill: 'Interior, HomeCare',
+      dailysal: 'Rp. 125.000,00',
+      imageAsset: 'image/kuliku4.png'),
+  ListKuli(
+      name: 'Hinata',
+      alamat: 'Semarang, Jawa Tengah',
+      skill: 'Yard, Build',
+      dailysal: 'Rp. 145.000,00',
+      imageAsset: 'image/kuliku5.png'),
+  ListKuli(
+      name: 'Makima',
+      alamat: 'Tangerang, Banten',
+      skill: 'Renovate, HomeCare',
+      dailysal: 'Rp. 165.000,00',
+      imageAsset: 'image/kuliku6.png'),
+];
