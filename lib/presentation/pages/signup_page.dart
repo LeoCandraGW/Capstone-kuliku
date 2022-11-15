dac00ea9eb410d82467e4cc52af2a47fc4ae573b
Leo Candra <leocandra1609@gmail.com>
2022-11-15 10:33:12 +0700

    image assets

--- lib/presentation/pages/signup_page.dart
+++ lib/presentation/pages/signup_page.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
 
-class SignUpPage extends StatelessWidget{
-  const SignUpPage({Key? key}):super(key: key);
-  
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: const <Widget>[
-          Text('data'),
+        children: <Widget>[
+          Expanded(
+            child: Image.asset(
+              'image/signup.jpg',
+              fit: BoxFit.cover,
+              height: double.infinity,
+              width: double.infinity,
+              alignment: Alignment.center,
+            ),
+          ),
+          
+          Container(
+            alignment: Alignment.topRight,
+            child: Image.asset(
+               'image/logo.png',
+               height: 135,
+               width: 135,
+            ),
+          ),
+          Container()
         ],
       ),
     );
