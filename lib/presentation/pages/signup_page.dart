944f33c2291dd77cd4908d2205af0f5daaa1d236
Leo Candra <leocandra1609@gmail.com>
2022-11-15 11:26:14 +0700

    sign up page

--- lib/presentation/pages/signup_page.dart
+++ lib/presentation/pages/signup_page.dart
@@ -17,16 +17,87 @@ class SignUpPage extends StatelessWidget {
               alignment: Alignment.center,
             ),
           ),
-          
-          Container(
-            alignment: Alignment.topRight,
-            child: Image.asset(
-               'image/logo.png',
-               height: 135,
-               width: 135,
-            ),
+          Column(
+            children: <Widget>[
+              Container(
+                alignment: Alignment.topRight,
+                child: Image.asset(
+                  'image/logo.png',
+                  height: 135,
+                  width: 135,
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerRight,
+                width: 319,
+                height: 182,
+                child: const Text(
+                  'Build Your Dream House And Make Over Your Room',
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerRight,
+                width: 319,
+                height: 64,
+                child: const Text(
+                  'find your best kuli and start your first brick with us!',
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Container(
+                width: 286,
+                height: 56,
+                margin: const EdgeInsets.only(top: 150),
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: const Color(0xffBADCFF),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                  ),
+                  onPressed: (() {}),
+                  child: const Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      color: Color(0xff000000),
+                      fontSize: 24,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const <Widget>[
+                    Text(
+                      'already have account ?',
+                      style: TextStyle(
+                        color: Color(0xFFFFFFFF),
+                      ),
+                    ),
+                    Text(
+                      'Log In',
+                      style: TextStyle(
+                        color: Color(0xFF75B4FF),
+                      ),
+                      ),
+                  ],
+                ),
+              ),
+            ],
           ),
-          Container()
         ],
       ),
     );
