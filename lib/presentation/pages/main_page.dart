92efb8776a51c056d07972e618c59b56cca4e1a0
Leo Candra <leocandra1609@gmail.com>
2022-11-16 14:17:56 +0700

    carousel slider main page

--- lib/presentation/pages/main_page.dart
+++ lib/presentation/pages/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class MainPage extends StatelessWidget {
   static const routeName = "/mainpage";
@@ -9,53 +10,79 @@ class MainPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xFF002f48),
       body: SafeArea(
-        child: Stack(
+        child: Column(
           children: <Widget>[
-            Row(
+            Stack(
               children: <Widget>[
-                 Padding(
-              padding: const EdgeInsets.only(
-                  top: 8.0, bottom: 8.0, left: 20.0, right: 8.0),
-              child: Stack(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(top: 4.0),
-                    child: Container(
-                      width: 300,
-                      height: 35,
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(20.0),
-                        ),
-                        color: Colors.white,
+                Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          top: 8.0, bottom: 8.0, left: 20.0, right: 8.0),
+                      child: Stack(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(top: 4.0),
+                            child: Container(
+                              width: 300,
+                              height: 35,
+                              decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(20.0),
+                                ),
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 300,
+                            height: 40,
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: 'Cari Layanan, Kuli & tujuan',
+                                prefixIcon: Icon(Icons.search),
+                                hintStyle: TextStyle(fontSize: 15.0),
+                              ),
+                            ),
+                          )
+                        ],
                       ),
                     ),
-                  ),
-                  const SizedBox(
-                    width: 300,
-                    height: 40,
-                    child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Cari Layanan, Kuli & tujuan',
-                      prefixIcon: Icon(Icons.search),
-                      hintStyle: TextStyle(fontSize: 15.0),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: IconButton(
+                        icon: const Icon(
+                          Icons.message_outlined,
+                          size: 35,
+                        ),
+                        color: Colors.white,
+                        onPressed: () {},
+                      ),
                     ),
-                  ),
-                  )
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: IconButton(
-                icon: const Icon(
-                  Icons.message_outlined,
-                  size: 35,),
-                color: Colors.white,
-                onPressed: () {},
-              ),
-            ),
+                  ],
+                ),
               ],
+            ),
+            CarouselSlider(
+              options: CarouselOptions(
+                height: 150.0,
+                autoPlay: true,
+                autoPlayInterval: Duration(seconds: 5),
+                ),
+              items: [1,2,3,4].map((i){
+                return Builder(
+                  builder: (BuildContext context){
+                    return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: BoxDecoration(
+                        color: Colors.amber
+                      ),
+                      child: Text('text $i', style: TextStyle(fontSize: 16.0),),
+                    );
+                  }
+                  );
+              }).toList(),
             ),
           ],
         ),
