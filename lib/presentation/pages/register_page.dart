0929a13a852279faac7c2a011200650b3c492245
unknown <wiramuhammadalif@gmail.com>
2022-11-16 16:06:42 +0700

    single

--- lib/presentation/pages/register_page.dart
+++ lib/presentation/pages/register_page.dart
@@ -12,197 +12,207 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: const Color(0xff002F48),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 30.0),
-            child: Center(
-              child: SizedBox(
-                width: 200,
-                height: 200,
-                child: Stack(children: <Widget>[
-                  Expanded(child: Image.asset('image/logo.png'))
-                ]),
+    return Flexible(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: const Color(0xff002F48),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 30.0),
+              child: Center(
+                child: SizedBox(
+                  width: 200,
+                  height: 200,
+                  child: Stack(children: <Widget>[
+                    Expanded(child: Image.asset('image/logo.png'))
+                  ]),
+                ),
               ),
             ),
-          ),
-          const Text(
-            'Register',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                color: Colors.white,
-                fontFamily: 'League Spartan',
-                fontSize: 46,
-                fontWeight: FontWeight.bold,
-                height: 1),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(16.0),
-            child: SizedBox(
-              width: 240,
-              height: 70,
+            const Flexible(
               child: Text(
-                'Create your account and start your building with us ',
+                'Register',
                 textAlign: TextAlign.center,
                 style: TextStyle(
                     color: Colors.white,
                     fontFamily: 'League Spartan',
-                    fontSize: 20,
-                    fontWeight: FontWeight.w300,
+                    fontSize: 46,
+                    fontWeight: FontWeight.bold,
                     height: 1),
               ),
             ),
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 0.0),
-                  child: Stack(
-                    children: <Widget>[
-                      Container(
-                        width: 304,
-                        height: 60,
-                        decoration: const BoxDecoration(
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(12),
-                              topRight: Radius.circular(12),
-                              bottomLeft: Radius.circular(12),
-                              bottomRight: Radius.circular(12),
-                            ),
-                            color: Colors.white),
-                        child: const Padding(
-                          padding: EdgeInsets.only(
-                              left: 8.0, top: 8, right: 8, bottom: 5),
-                          child: TextField(
-                            decoration: InputDecoration(
-                                hintText: 'Username',
-                                prefixIcon: Icon(Icons.person)),
-                          ),
-                        ),
-                      ),
-                    ],
+            const Flexible(
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: SizedBox(
+                  width: 240,
+                  height: 70,
+                  child: Text(
+                    'Create your account and start your building with us ',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'League Spartan',
+                        fontSize: 20,
+                        fontWeight: FontWeight.w300,
+                        height: 1),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 20.0),
-                  child: Stack(
-                    children: <Widget>[
-                      Container(
-                        width: 304,
-                        height: 60,
-                        decoration: const BoxDecoration(
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(12),
-                              topRight: Radius.circular(12),
-                              bottomLeft: Radius.circular(12),
-                              bottomRight: Radius.circular(12),
-                            ),
-                            color: Colors.white),
-                        child: const Padding(
-                          padding: EdgeInsets.only(
-                              left: 8.0, top: 8, right: 8, bottom: 5),
-                          child: TextField(
-                            decoration: InputDecoration(
-                                hintText: 'Email Address',
-                                prefixIcon: Icon(Icons.email)),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 20.0),
-                  child: Stack(
-                    children: <Widget>[
-                      Container(
-                        width: 304,
-                        height: 60,
-                        decoration: const BoxDecoration(
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(12),
-                              topRight: Radius.circular(12),
-                              bottomLeft: Radius.circular(12),
-                              bottomRight: Radius.circular(12),
-                            ),
-                            color: Colors.white),
-                        child: const Padding(
-                          padding: EdgeInsets.only(
-                              left: 8.0, top: 8, right: 8, bottom: 5),
-                          child: TextField(
-                            decoration: InputDecoration(
-                                hintText: 'Password',
-                                prefixIcon: Icon(Icons.lock)),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 20.0),
-                  child: Stack(
-                    children: <Widget>[
-                      Container(
-                        width: 304,
-                        height: 60,
-                        decoration: const BoxDecoration(
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(12),
-                              topRight: Radius.circular(12),
-                              bottomLeft: Radius.circular(12),
-                              bottomRight: Radius.circular(12),
-                            ),
-                            color: Colors.white),
-                        child: const Padding(
-                          padding: EdgeInsets.only(
-                              left: 8.0, top: 8, right: 8, bottom: 5),
-                          child: TextField(
-                            decoration: InputDecoration(
-                                hintText: 'Confirm Password',
-                                prefixIcon: Icon(Icons.lock)),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 40.0),
-                        child: Container(
-                          width: 304,
-                          height: 56,
-                          margin: const EdgeInsets.only(top: 100),
-                          child: TextButton(
-                            style: TextButton.styleFrom(
-                              backgroundColor: const Color(0xff35516E),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(46),
+              ),
+            ),
+            Flexible(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                keyboardDismissBehavior:
+                    ScrollViewKeyboardDismissBehavior.onDrag,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 0.0),
+                      child: Stack(
+                        children: <Widget>[
+                          Container(
+                            width: 304,
+                            height: 60,
+                            decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(12),
+                                  topRight: Radius.circular(12),
+                                  bottomLeft: Radius.circular(12),
+                                  bottomRight: Radius.circular(12),
+                                ),
+                                color: Colors.white),
+                            child: const Padding(
+                              padding: EdgeInsets.only(
+                                  left: 8.0, top: 8, right: 8, bottom: 5),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    hintText: 'Username',
+                                    prefixIcon: Icon(Icons.person)),
                               ),
                             ),
-                            onPressed: () {
-                              Navigator.pushNamed(context, LoginPage.routeName);
-                            },
-                            child: const Text(
-                              'REGISTER',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.bold,
-                                  fontFamily: 'DM Sans'),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0),
+                      child: Stack(
+                        children: <Widget>[
+                          Container(
+                            width: 304,
+                            height: 60,
+                            decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(12),
+                                  topRight: Radius.circular(12),
+                                  bottomLeft: Radius.circular(12),
+                                  bottomRight: Radius.circular(12),
+                                ),
+                                color: Colors.white),
+                            child: const Padding(
+                              padding: EdgeInsets.only(
+                                  left: 8.0, top: 8, right: 8, bottom: 5),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    hintText: 'Email Address',
+                                    prefixIcon: Icon(Icons.email)),
+                              ),
                             ),
                           ),
-                        ),
+                        ],
                       ),
-                    ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0),
+                      child: Stack(
+                        children: <Widget>[
+                          Container(
+                            width: 304,
+                            height: 60,
+                            decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(12),
+                                  topRight: Radius.circular(12),
+                                  bottomLeft: Radius.circular(12),
+                                  bottomRight: Radius.circular(12),
+                                ),
+                                color: Colors.white),
+                            child: const Padding(
+                              padding: EdgeInsets.only(
+                                  left: 8.0, top: 8, right: 8, bottom: 5),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    hintText: 'Password',
+                                    prefixIcon: Icon(Icons.lock)),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0),
+                      child: Stack(
+                        children: <Widget>[
+                          Container(
+                            width: 304,
+                            height: 60,
+                            decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(12),
+                                  topRight: Radius.circular(12),
+                                  bottomLeft: Radius.circular(12),
+                                  bottomRight: Radius.circular(12),
+                                ),
+                                color: Colors.white),
+                            child: const Padding(
+                              padding: EdgeInsets.only(
+                                  left: 8.0, top: 8, right: 8, bottom: 5),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    hintText: 'Confirm Password',
+                                    prefixIcon: Icon(Icons.lock)),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 40.0),
+              child: Container(
+                width: 304,
+                height: 56,
+                margin: const EdgeInsets.only(top: 100),
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: const Color(0xff35516E),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(46),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, LoginPage.routeName);
+                  },
+                  child: const Text(
+                    'REGISTER',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'DM Sans'),
                   ),
                 ),
-              ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
