d1620ed77ace305f37ac23cd015c70830660dad2
unknown <wiramuhammadalif@gmail.com>
2022-11-23 11:18:17 +0700

    list detail

--- lib/presentation/widgets/kuli_card.dart
+++ lib/presentation/widgets/kuli_card.dart
@@ -8,68 +8,127 @@ class KuliCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemBuilder: (context, index){
+      itemBuilder: (context, index) {
         final ListKuli kuli = listKuliku[index];
-        return InkWell(
-          onTap: (() {
-            Navigator.push(context, MaterialPageRoute(builder: (context){
-              return DetailPage(kuli: kuli);
-            }));
-          }),
-          child: Stack(
-            alignment: Alignment.bottomLeft,
-            children: [
-              Card(
-                child: Container(
-                  margin: const EdgeInsets.only(
-                    left: 16 + 80 + 16,
-                  bottom: 8,
-                  right: 8,
-                  ),
-                  width: double.infinity,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        kuli.name,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
+        return SingleChildScrollView(
+          child: InkWell(
+            onTap: (() {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return DetailPage(kuli: kuli);
+              }));
+            }),
+            child: Stack(
+              alignment: Alignment.bottomLeft,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: double.infinity,
+                    height: 114,
+                    decoration: const BoxDecoration(
+                      color: Color(0xff9BB0B3),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
+                        bottomLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20),
                       ),
-                       const SizedBox(height: 16),
-                      Text(
-                        kuli.alamat,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      const SizedBox(height: 16),
-                      Text(
-                        kuli.skill,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ],
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          decoration: const BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.black54,
+                                  offset: Offset(0, 4),
+                                  blurRadius: 4)
+                            ],
+                          ),
+                          margin: const EdgeInsets.only(
+                              left: 20, bottom: 5, top: 5, right: 10),
+                          child: ClipRRect(
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(8)),
+                            child: Image.asset(
+                              kuli.imageAsset,
+                              width: 80,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            width: 230,
+                            height: 75,
+                            decoration: const BoxDecoration(
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(6),
+                                topRight: Radius.circular(6),
+                                bottomLeft: Radius.circular(6),
+                                bottomRight: Radius.circular(6),
+                              ),
+                              color: Color.fromRGBO(217, 217, 217, 1),
+                              boxShadow: [
+                                BoxShadow(
+                                    color: Colors.black54,
+                                    offset: Offset(0, 4),
+                                    blurRadius: 4)
+                              ],
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                color: const Color(0xffD9D9D9),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                          top: 0.0, left: 8),
+                                      child: Text(
+                                        kuli.name,
+                                        style: const TextStyle(
+                                            fontSize: 20.0,
+                                            fontFamily: 'DM Sans'),
+                                        maxLines: 1,
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                          top: 4.0, left: 8),
+                                      child: Text(
+                                        kuli.alamat,
+                                        maxLines: 1,
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                          top: 6.0, left: 8),
+                                      child: Text(
+                                        kuli.skill,
+                                        maxLines: 1,
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(
-                  left: 16,
-                  bottom: 16,
-                ),
-                child: ClipRRect(
-                  borderRadius: const BorderRadius.all(Radius.circular(8)),
-                  child: Image.asset(
-                    kuli.imageAsset,
-                    width: 70,
-                  ),
-                ),
-              ),
-            ],
-
+              ],
+            ),
           ),
         );
       },
       itemCount: listKuliku.length,
-      );
+    );
   }
 }
