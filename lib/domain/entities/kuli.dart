feb94d84be12b2ad383a1d1a440b9c4db0412234
unknown <wiramuhammadalif@gmail.com>
2022-11-29 10:05:38 +0700

    bloc

---
+++ lib/domain/entities/kuli.dart
@@ -0,0 +1,44 @@
+import 'package:equatable/equatable.dart';
+
+class Kuli extends Equatable {
+  Kuli({
+    required this.id,
+    required this.username,
+    required this.email,
+    required this.password,
+    required this.skill,
+    required this.nohp,
+    required this.dailysal,
+    required this.image,
+    required this.deskripsi,
+    required this.nik,
+    required this.alamat,
+  });
+
+  int id;
+  String username;
+  String email;
+  String password;
+  String skill;
+  String nohp;
+  String dailysal;
+  String image;
+  String deskripsi;
+  String nik;
+  String alamat;
+
+  @override
+  List<Object?> get props => [
+        id,
+        username,
+        email,
+        password,
+        skill,
+        nohp,
+        dailysal,
+        image,
+        deskripsi,
+        nik,
+        alamat,
+      ];
+}
---
+++ lib/presentation/bloc/kuli_bloc.dart
@@ -0,0 +1,24 @@
+import 'package:capstone_kuliku/domain/usecases/get_kuli_list.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+
+import '../../domain/entities/kuli.dart';
+part 'kuli_event.dart';
+part 'kuli_state.dart';
+
+class GetKuliListBloc extends Bloc<KuliBlocEvent, KuliBlocState> {
+  final GetKuliList _getKuliList;
+
+  GetKuliListBloc(this._getKuliList) : super(KuliEmpty()) {
+    on<FetchKuliList>((event, emit) async {
+      emit(KuliLoading());
+      final result = await _getKuliList.execute();
+
+      result.fold((failure) {
+        emit(KuliHasError(failure.message));
+      }, (kulidata) {
+        emit(KuliHasData(kulidata));
+      });
+    });
+  }
+}
